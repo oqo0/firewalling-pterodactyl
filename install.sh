d2051650bdac99669c49066d701059319049f34d
Roman <oqqqqqqqqqqq@gmail.com>
2022-08-06 18:51:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 COPY_FROM="${SCRIPT_PATH}/ufw/after.rules"
 COPY_TO="/etc/ufw/after.rules"
 
+# making a backup copy
+cp -R "${COPY_TO}" "${COPY_TO}.copy"
+
 # recursively copy files
 cp -R "${COPY_FROM}" "${COPY_TO}"
 
